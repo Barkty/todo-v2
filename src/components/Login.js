1e12ed6450240ca2fe8b71e5bf762b70a8dac19e
Gbemisola Kotoye <barkty@yahoo.com>
2021-06-21 18:11:45 +0100

    User personification added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
 import './styles.css'
 
 
-const Login = () => {
- return(
+class Login extends Component {
+    constructor(props){
+	super(props)
+
+	this.state = {
+	    name: '',
+	    password: ''
+	}
+
+	changeHandler = e => {
+	    this.setState(e.target.value)
+	}
+
+	submitHandler() {
+	    e.preventDefault()
+	    alert(this.state)
+	    axios.post('https://user-manager-')
+	}
+    }
+ 
+    return(
      <div className='form-div mt-5'>
-         <form className='form-signin m-5'>
+         <form className='form-signin m-5' onSubmit={this.submitHandler}>
           <h1 className="h3 mb-3 text-center font-weight-normal">Login</h1>
             <label className="sr-only">Username</label>
-            <input type='email' className='form-control mb-4' placeholder='Enter email'/>
+             <input type='email' name='name' value={this.state.name} className='form-control mb-4' placeholder='Enter email' onChange={this.changeHandler}/>
             <label >Password</label>
-            <input type='password' className='form-control' placeholder='********'/>
+             <input type='password' className='form-control' name='password' value={this.state.password} placeholder='********' onChange={this.changeHandler}/>
             <button className="btn btn-lg btn-success btn-block mt-3" type="submit">Login</button>
          </form>
      </div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -7,54 +7,63 @@ class Register extends Component{
         super(props)
 
         this.state = {
-            
+            userId: '',
+	    email: '',
+	    password: '',
+	    confirm: ''
         }
     }
     
     
     
     
-    const [userId, setUserId] = useState('')
+    //const [userId, setUserId] = useState('')
     // const [username, setUserName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirm, setConfirm] = useState('')
+    //const [email, setEmail] = useState('')
+    //const [password, setPassword] = useState('')
+    //const [confirm, setConfirm] = useState('')
 
-    const submitHandler = e => {
+    changeHandler = e => {
+	this.setState({[e.target.name]: e.target.value})
+    }
+    submitHandler = e => {
         e.preventDefault()
-        console.log(newUser)
+        alert(this.state)
+	axios.post('https://user-manager-three.vercel.app/api/', this.state)
+	    .then(response => {
+		console.log(response)
+	    } )
+	    .catch(error => {
+		console.log(error)
+	    })
     };
 
-    const registerForm = () => {
+    registerForm = () => {
         if(!(userId || password || confirm || email))  {
             alert('Please register an account')
             return;
         }
-    }
+    };
     
-    if(confirm !== password) {
-        alert('Password doesn\'t match')
-    }
-
     //Concatenate the user info
     //const registeredUser = user.concat({userId, email, password})
    // newUser(registeredUser);
 
  return( 
-    //  const {userId, username, email, password, confirm} = this.props;
+    //const {userId, email, password, confirm} = this.state
 
      <div className='form-div mt-5'>
-         <form className='form-signin m-5' onSubmit={submitHandler}>
+         <form className='form-signin m-5' onSubmit={this.submitHandler}>
           <h1 className="h3 mb-3 text-center font-weight-normal">Register</h1>
             <label className="sr-only">Username</label>
-            <input type='text' className='form-control mb-4' name='userId' value={userId} placeholder='Enter username' onChange={(e) => {setUserId({[e.target.name]: e.target.value})}}/>
+             <input type='text' className='form-control mb-4' name='userId' value={this.userId} placeholder='Enter username' onChange={this.changeHandler}/>
             <label className="sr-only">Email address</label>
-            <input type='email' className='form-control mb-4' name='email' value={email}placeholder='Enter email' onChange={(e) => {setEmail({[e.target.name]: e.target.value})}}/>
+             <input type='email' className='form-control mb-4' name='email' value={this.email} placeholder='Enter email' onChange={this.changeHandler}/>
             <label className="sr-only">Password</label>
-            <input type='password' className='form-control mb-4' name='password' value={password} placeholder='Enter password' onChange={(e) => {setPassword({[e.target.name]: e.target.value})}}/>
+            <input type='password' className='form-control mb-4' name='password' value={this.password} placeholder='Enter password' onChange={this.changeHandler}/>
             <label className="sr-only">Confirm Password</label>
-            <input type='password' className='form-control' name='confirm' value={confirm} placeholder='********' onChange={(e) => {setConfirm({[e.target.name]: e.target.value})}}/>
-            <button className="btn btn-lg btn-success btn-block mt-3" type="submit" onClick={registerForm}>Register</button>
+            <input type='password' className='form-control' name='confirm' value={this.confirm} placeholder='********' onChange={this.changeHandler}/>
+            <button className="btn btn-lg btn-success btn-block mt-3" type="submit" onClick={this.registerForm}>Register</button>
             <p>Have an account? <a href='./Login.js'>Login</a></p>
          </form>
      </div>
